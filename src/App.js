2b87dacfa479185ca063926c10a2081276465cb1
“[Rajashekar <[raju10naidu@gmail.com]>
2021-11-05 10:51:11 +0530

    Integrated Expense-App into larger App

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import ExpenseApp from './Expenses-App/ExpenseApp';
 
 const App = () => {
+  //to show and hide Expense App
   const [showExpenseApp, setShowExpenseApp] = useState(false);
   const openExpenseAppUI = () => setShowExpenseApp(true);
   const closeExpenseAppUI = () => setShowExpenseApp(false);
 
+  //to show and hide Goals App
+  const [showGoalsApp, setShowGoalsApp] = useState(false);
+  const openGoalsAppUI = () => setShowGoalsApp(true);
+  const closeGoalsAppUI = () => setShowGoalsApp(false);
+
   return (
-    <div className="button-container">
-      {!showExpenseApp && <button onClick={openExpenseAppUI}>Open Expense App</button>}
-      {showExpenseApp && <ExpenseApp control={closeExpenseAppUI} />}
+    <div>
+      <div className="btn-group">
+        {/* Expense APP  */}
+        {!showExpenseApp && !showGoalsApp && <button className="button" onClick={openExpenseAppUI}>Open Expense App</button>}
+        {/* Goals APP  */}
+        {!showGoalsApp && !showExpenseApp && <button className="button" onClick={openGoalsAppUI}>Open Goals App</button>}
+      </div>
+      <div>
+        {/* Expense APP  */}
+        {showExpenseApp && <ExpenseApp control={closeExpenseAppUI} />}
+        {/* Goals APP  */}
+        {showGoalsApp && <ExpenseApp control={closeGoalsAppUI} />}
+      </div>
     </div>
   );
 }
