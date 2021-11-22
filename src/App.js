a36562c10837bff8f6b4a3020f76a2c6c1aa6ee2
Rajashekar G Naidu <raju10naidu@gmail.com>
2021-11-22 21:25:50 +0530

    Login-App initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ExpenseApp from './Expenses-App/ExpenseApp';
 import GoalsApp from './Goals-App/GoalsApp';
 import PersonalDetailsApp from './Personal-Details-App/PersonalDetailsApp';
+import LoginApp from './Login-App/LoginApp';
 
 const App = () => {
   //to show and hide Expense App
@@ -15,20 +16,27 @@ const App = () => {
   const openGoalsAppUI = () => setShowGoalsApp(true);
   const closeGoalsAppUI = () => setShowGoalsApp(false);
 
-    //to show and hide Personal Details App
-    const [showDetailsApp, setShowDetailsApp] = useState(false);
-    const openPersonalDetailsAppUI = () => setShowDetailsApp(true);
-    const closePersonalDetailsAppUI = () => setShowDetailsApp(false);
+  //to show and hide Personal Details App
+  const [showDetailsApp, setShowDetailsApp] = useState(false);
+  const openPersonalDetailsAppUI = () => setShowDetailsApp(true);
+  const closePersonalDetailsAppUI = () => setShowDetailsApp(false);
+
+  //to show and hide Login App
+  const [showLoginApp, setShowLoginApp] = useState(false);
+  const openLoginAppUI = () => setShowLoginApp(true);
+  const closeLoginAppUI = () => setShowLoginApp(false);
 
   return (
     <React.Fragment>
       <div className="btn-group">
         {/* Expense APP  */}
-        {!showExpenseApp && !showGoalsApp && !showDetailsApp && <button className="button" onClick={openExpenseAppUI}>Open Expense App</button>}
+        {!showExpenseApp && !showGoalsApp && !showDetailsApp && !showLoginApp && <button className="button" onClick={openExpenseAppUI}>Open Expense App</button>}
         {/* Goals APP  */}
-        {!showGoalsApp && !showExpenseApp && !showDetailsApp && <button className="button" onClick={openGoalsAppUI}>Open Goals App</button>}
+        {!showGoalsApp && !showExpenseApp && !showDetailsApp && !showLoginApp && <button className="button" onClick={openGoalsAppUI}>Open Goals App</button>}
         {/* Personal Details APP  */}
-        {!showGoalsApp && !showExpenseApp && !showDetailsApp && <button className="button" onClick={openPersonalDetailsAppUI}>Open Personal Details App</button>}
+        {!showGoalsApp && !showExpenseApp && !showDetailsApp && !showLoginApp && <button className="button" onClick={openPersonalDetailsAppUI}>Open Personal Details App</button>}
+        {/* Login APP  */}
+        {!showGoalsApp && !showExpenseApp && !showDetailsApp && !showLoginApp && <button className="button" onClick={openLoginAppUI}>Open Login App</button>}
       </div>
       <React.Fragment>
         {/* Expense APP  */}
@@ -37,6 +45,8 @@ const App = () => {
         {showGoalsApp && <GoalsApp control={closeGoalsAppUI} />}
         {/* Personal Details APP  */}
         {showDetailsApp && <PersonalDetailsApp control={closePersonalDetailsAppUI} />}
+        {/* Login APP  */}
+        {showLoginApp && <LoginApp control={closeLoginAppUI} />}
       </React.Fragment>
     </React.Fragment>
   );
---
+++ src/Login-App/LoginApp.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import "./LoginApp.module.css";
+import MainHeader from "./components/MainHeader/MainHeader";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+
+const LoginApp = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+
+};
+
+export default LoginApp;
