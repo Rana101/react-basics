00c137a93d11cccd7ad7a1afd3bbadb017b64ae7
Rajashekar G Naidu <raju10naidu@gmail.com>
2021-11-22 20:37:26 +0530

    Added close App button to Personal-details App

--- src/Personal-Details-App/PersonalDetailsApp.js
+++ src/Personal-Details-App/PersonalDetailsApp.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import AddUser from "./components/Users/AddUser";
 import styles from './PersonalDetailsApp.module.css';
 import UsersList from "./components/Users/UsersList";
+import Button from "./components/UI/Button/Button";
 
 const PersonalDetailsApp = (props) => {
 	const [userData, setUserData] =	useState([]);
@@ -16,6 +17,9 @@ const PersonalDetailsApp = (props) => {
 		<div className={styles.wrapper} >
 			<AddUser onAddUser={addUserHandler} />
 			<UsersList userData={userData} />	
+			<div className={styles['button-container']}>
+				<Button type="button" onClick={props.control} >Close Goals App</Button>
+			</div>
 		</div>
 	);
 };
